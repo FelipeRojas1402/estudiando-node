0653a0c335dc527b8ae0d2cfe71e11fd37a8551e
Felipe Alejandro Rojas Lopez <felipe.rojas1994@gmail.com>
2021-03-23 20:01:51 -0500

    Se inicio con la creación del menu, y se usaron promesas y readline

---
+++ 04-tareas-hacer/helpers/mensajes.js
@@ -0,0 +1,57 @@
+const { resolve } = require('path');
+
+require('colors');
+
+
+const mostrarMenu = () => {
+
+    return new Promise(resolve =>{
+        console.clear();
+        console.log('\n=========================='.green)
+        console.log('==seleccione una opción===')
+        console.log('==========================\n'.green)
+
+        console.log(`${'1.'.green} Crear tareas`);
+        console.log(`${'2.'.green} Listar tareas`);
+        console.log(`${'3.'.green} Listar tareas comletadas`);
+        console.log(`${'4.'.green} Listar tareas pendientes`);
+        console.log(`${'5.'.green} Completar tareas`);
+        console.log(`${'6.'.green} Borrar una tarea`);
+        console.log(`${'0.'.green} Salir\n`);
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        readline.question('Seleccione una opción: ', (opt) =>{
+            readline.close();
+            resolve(opt);
+        })
+    });
+
+    
+}
+
+const pausa = () =>{
+
+    return new Promise(resolve =>{
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question(`Presione ${'Enter'.green} para continuar`, (opt) =>{
+            readline.close();
+            resolve();
+        })
+    });
+
+    
+
+}
+
+module.exports = {
+    mostrarMenu, 
+    pausa
+}
